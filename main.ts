b8c9107f7c7afcbf19dffb53ecb1673ba37abd2e
jcfsilva <69762221+jcfsilva@users.noreply.github.com>
2021-02-14 15:26:30 +0100

    Deleted superfulous variables

--- main.ts
+++ main.ts
@@ -6,21 +6,18 @@ let bird_y = 2
 let bird_x = 1
 music.setBuiltInSpeakerEnabled(true)
 game.setScore(0)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     wall = randint(2, 4)
     wallx = 4
     for (let index = 0; index < 5; index++) {
         wally = wall
-        for (let index2 = 0; index2 < 6; index2++) {
-            if (index2 > wally) {
-                led.plot(wallx, index2)
+        for (let index = 0; index <= 5; index++) {
+            if (index > wally) {
+                led.plot(wallx, index)
             }
-            
-            if (index2 < wally - 1) {
-                led.plot(wallx, index2)
+            if (index < wally - 1) {
+                led.plot(wallx, index)
             }
-            
         }
         if (input.buttonIsPressed(Button.A)) {
             bird_y += -1
@@ -30,15 +27,12 @@ basic.forever(function on_forever() {
             if (bird_y < 0) {
                 bird_y = 0
             }
-            
             bird_y += 0.5
             if (bird_y > 4) {
                 bird_y = 4
             }
-            
         }
-        
-        led.plot(bird_x, bird_y)
+        led.plot(bird_x, Math.round(bird_y))
         basic.pause(500)
         if (bird_x == wallx) {
             if (Math.round(bird_y) == wally || Math.round(bird_y) == wally - 1) {
@@ -50,20 +44,16 @@ basic.forever(function on_forever() {
                 game.setScore(score)
                 game.gameOver()
             }
-            
         }
-        
-        led.unplot(bird_x, bird_y)
+        led.unplot(bird_x, Math.round(bird_y))
         wally = wall
-        for (let index3 = 0; index3 < 6; index3++) {
-            if (index3 > wally) {
-                led.unplot(wallx, index3)
+        for (let index = 0; index <= 5; index++) {
+            if (index > wally) {
+                led.unplot(wallx, index)
             }
-            
-            if (index3 < wally - 1) {
-                led.unplot(wallx, index3)
+            if (index < wally - 1) {
+                led.unplot(wallx, index)
             }
-            
         }
         wallx += -1
     }
